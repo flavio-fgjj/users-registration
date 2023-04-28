5609d15924bf86a6413a7250eb9fd5a2c5935a91
Andre Cordeiro <andrecordeiro.oliveira@hotmail.com>
2023-04-28 13:48:07 -0300

    feat: general adjustment

--- app/router/app.routes.tsx
+++ app/router/app.routes.tsx
@@ -1,34 +1,27 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image, Pressable } from 'react-native';
+import {Text, View, StyleSheet, Image, Pressable} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {useDispatch} from 'react-redux';
-
-
-// icons
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 // pages
 import HomeScreen from '@screens/home';
 import ContactScreen from '@screens/contact';
 
 // styles
-import { colors } from 'app/colors';
+import {colors} from 'app/colors';
 
 // assets
 import logo from '../assets/registration.png';
 import logout from '../assets/logout.png';
 
 // store
-import { signOut } from 'app/store/features/auth';
+import {signOut} from 'app/store/features/auth';
 
 const Stack = createNativeStackNavigator();
 
-type Props = NativeStackScreenProps<any>;
-
 export type RootStackParamList = {
-  HomeScreen: undefined;
-  ContactScreen: undefined;
+  Home: undefined;
+  Contact: undefined;
 };
 
 const styles = StyleSheet.create({
@@ -37,73 +30,71 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     width: '100%',
-    paddingRight: 30
+    paddingRight: 30,
   },
   logoHeaderHome: {
     width: 40,
-    height: 40
+    height: 40,
   },
   logoutImg: {
     width: 30,
-    height: 32
+    height: 32,
   },
   textHeaderHome: {
     color: '#000',
     fontSize: 18,
-    fontWeight: 'bold', 
-    paddingLeft: 15 
+    fontWeight: 'bold',
+    paddingLeft: 15,
   },
   viewLeft: {
     flexDirection: 'row',
-    alignItems: 'center', 
-  }
+    alignItems: 'center',
+  },
 });
 
 function HeaderHome() {
   const dispatch = useDispatch();
 
   function loginOut() {
-    dispatch(
-      signOut()
-    );
+    dispatch(signOut());
   }
 
   return (
     <View style={styles.headerHomeContainer}>
       <View style={styles.viewLeft}>
-        <Image source={logo} style={styles.logoHeaderHome}></Image>
+        <Image source={logo} style={styles.logoHeaderHome} />
         <Text style={styles.textHeaderHome}>Nome do Usuário</Text>
       </View>
 
-        <Pressable onPress={() => loginOut()}>
-          <Image source={logout} style={styles.logoutImg}></Image>
-        </Pressable>
+      <Pressable onPress={() => loginOut()}>
+        <Image source={logout} style={styles.logoutImg} />
+      </Pressable>
     </View>
-  )
+  );
 }
 
 const AppRoutes = () => {
   return (
-    <Stack.Navigator initialRouteName="HomeScreen">
+    <Stack.Navigator initialRouteName="Home">
       <Stack.Screen
         name="Home"
         component={HomeScreen}
         options={{
           headerShown: true,
-          headerTitle: (props) => <HeaderHome />
+          headerTitle: () => <HeaderHome />,
         }}
       />
       <Stack.Screen
-        name="ContactScreen"
+        name="Contact"
         component={ContactScreen}
         options={{
-          headerShown: true, 
-          headerTintColor: '#fff', headerStyle: {
+          title: 'Contato',
+          headerShown: true,
+          headerTintColor: '#fff',
+          headerStyle: {
             backgroundColor: colors.primary,
-          }
+          },
         }}
-        
-        
       />
     </Stack.Navigator>
   );
--- app/screens/home/index.tsx
+++ app/screens/home/index.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import {View} from 'react-native';
 
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 // styles
 import {styles} from './styles';
 
 // components
 import FAB from '@components/FAB';
-import { RootStackParamList } from '@router/app.routes';
 
-type HomeScreenProps = NativeStackScreenProps<RootStackParamList, "HomeScreen">;
+type HomeScreenProps = NativeStackScreenProps<any>;
 
-const Home: React.FC<HomeScreenProps> = (props) => {
-  
+const Home: React.FC<HomeScreenProps> = props => {
   return (
     <View style={styles.container}>
-      <FAB onPress={() => props.navigation.push("ContactScreen")} title="+" />
+      <FAB onPress={() => props.navigation.push('Contact')} title="+" />
     </View>
   );
-}
-
+};
 
 export default Home;
